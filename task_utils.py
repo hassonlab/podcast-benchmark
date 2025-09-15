0e6594c4c07847b51af089caabfff492c19a8e6a
Zach Paris <zparis99@gmail.com>
2025-09-15 00:01:08 -0400

    fix nltk usage

--- task_utils.py
+++ task_utils.py
@@ -1,5 +1,6 @@
 import os
 
+import nltk
 from nltk.stem import WordNetLemmatizer as wl
 import pandas as pd
 from sklearn.decomposition import PCA
@@ -27,6 +28,8 @@ def word_embedding_decoding_task(data_params: DataParams):
         Tuple[pd.DataFrame, np.ndarray]: A DataFrame containing word-level information (word, start time, end time),
         and a NumPy array of corresponding word-level embeddings under the header target.
     """
+    nltk.download("wordnet")
+
     transcript_path = os.path.join(
         data_params.data_root, "stimuli/gpt2-xl/transcript.tsv"
     )
