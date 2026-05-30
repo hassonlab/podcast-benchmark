0856efb75300131941176c5d2435e39089a3edc9
Zach Paris <zparis99@gmail.com>
2026-05-30 11:17:27 -0400

    up mem per cpu

--- submit.sh
+++ submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=64G
+#SBATCH --mem-per-cpu=128G
 #SBATCH --gres=gpu:1
 #SBATCH --time=15:00:00
 #SBATCH --output='./logs/%x.out'
